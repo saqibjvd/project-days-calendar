a3f8afd29d25db13c367416c540bfdf8e13ff4fc
Emilianouz <135679131+Emilianouz@users.noreply.github.com>
2025-10-12 18:41:59 +0100

    -Basic HTML skeleton with: header, section to controls and section for calendar. -Basic Styling to create grid for calendar. -Comment in code of web.mjs

--- web.mjs
+++ web.mjs
@@ -7,5 +7,6 @@ import { getGreeting } from "./common.mjs";
 import daysData from "./days.json" with { type: "json" };
 
 window.onload = function() {
-    document.querySelector("body").innerText = `${getGreeting()} - there are ${daysData.length} known days`;
+ //   document.querySelector("body").innerText = `${getGreeting()} - there are ${daysData.length} known days`;
+
 }
