d4671220521a8cce1a139ccc4851effce796aa9c
Emilianouz <135679131+Emilianouz@users.noreply.github.com>
2025-10-13 18:53:46 +0100

    -Function to render days in calendar (basic 30 days). -Styling to fit days in calendar. -Function to fill Month selector.

--- web.mjs
+++ web.mjs
@@ -6,7 +6,36 @@
 import { getGreeting } from "./common.mjs";
 import daysData from "./days.json" with { type: "json" };
 
+const months = [ "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"];
+
 window.onload = function() {
  //   document.querySelector("body").innerText = `${getGreeting()} - there are ${daysData.length} known days`;
+const daysContainer = document.getElementById("days-container");
 
+function renderCalendar(year,month){
+    daysContainer.innerHTML = "";
+ // call function that switch days from months
+    for (let i = 1; i <= 30 ; i ++){
+        const day = document.createElement("div");
+        day.textContent = i;
+        daysContainer.appendChild(day);
+    }
+}
+ fillMonthSelector()
+//fillYearSelector()
+const today = new Date();
+renderCalendar(today.getFullYear(),today.getMonth());
+}
+
+function fillMonthSelector() {
+    const selectMonth = document.getElementById("select-month");
+    selectMonth.innerHTML = "";
+
+  months.forEach((month, index) => {
+    const option = document.createElement("option");
+    option.textContent = month;
+    option.value = index;
+    selectMonth.appendChild(option);
+  });
 }
